21529699d9f0c004277e7a526b50bb90a29f0fd9
AHMET1346z <145866626+AHMET1346z@users.noreply.github.com>
2024-05-01 18:58:02 +0300

    Update speedtest.py

--- AnonXMusic/plugins/tools/speedtest.py
+++ AnonXMusic/plugins/tools/speedtest.py
@@ -25,7 +25,7 @@ def testspeed(m, _):
     return result
 
 
-@app.on_message(filters.command(["hiztest"]) & SUDOERS)
+@app.on_message(filters.command(["bdndkd"]) & SUDOERS)
 @language
 async def speedtest_function(client, message: Message, _):
     m = await message.reply_text(_["server_11"])
