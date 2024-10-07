a0b82b26ec01615a4192446e9682c75d9986e5eb
AHMET1346z <145866626+AHMET1346z@users.noreply.github.com>
2024-10-08 01:21:53 +0300

    Update logger.py

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -3,18 +3,19 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 from AnonXMusic import app
 from config import LOGGER_ID
 
+
 @app.on_chat_member_updated()
 async def handle_chat_member_update(client, update):
-    # Botun gruba eklenmesi durumu
-    if update.new_chat_member.status == ChatMemberStatus.MEMBER:
+    # Botun gruptan atılması durumu
+    if update.new_chat_member.status == ChatMemberStatus.KICKED:
         log_text = f"""
-<b>AnonXMusic Bot Gruba Eklendi!</b>
+<b>AnonXMusic Bot Gruptan Atıldı!</b>
 
 <b>Grup ID:</b> <code>{update.chat.id}</code>
 <b>Grup Adı:</b> {update.chat.title}
 <b>Grup Linki:</b> @{update.chat.username}
 
-<b>Ekleme Yapan Kişi:</b> {update.from_user.mention}
+<b>Atan Kişi:</b> {update.from_user.mention}
 <b>Kişi ID:</b> <code>{update.from_user.id}</code>
 """
         try:
@@ -27,16 +28,19 @@ async def handle_chat_member_update(client, update):
         except Exception as e:
             print(f"Log mesajı gönderilemedi: {e}")
 
-    # Botun gruptan atılması durumu
-    elif update.new_chat_member.status == ChatMemberStatus.KICKED:
+        # Burada botu gruptan çıkaracak bir işlem yok, 
+        # çünkü Telegram API, botun kendisini koruma işlevi sağlamaz.
+        return
+    # Botun gruba eklenmesi durumu
+    elif update.new_chat_member.status == ChatMemberStatus.MEMBER:
         log_text = f"""
-<b>AnonXMusic Bot Gruptan Atıldı!</b>
+<b>AnonXMusic Bot Gruba Eklendi!</b>
 
 <b>Grup ID:</b> <code>{update.chat.id}</code>
 <b>Grup Adı:</b> {update.chat.title}
 <b>Grup Linki:</b> @{update.chat.username}
 
-<b>Atan Kişi:</b> {update.from_user.mention}
+<b>Ekleme Yapan Kişi:</b> {update.from_user.mention}
 <b>Kişi ID:</b> <code>{update.from_user.id}</code>
 """
         try:
