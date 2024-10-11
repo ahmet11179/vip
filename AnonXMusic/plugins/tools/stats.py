8bcb771d69f8f93635656f93b1027a5888ea21f7
AHMET1346z <145866626+AHMET1346z@users.noreply.github.com>
2024-10-11 18:24:37 +0300

    Update stats.py

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -1,13 +1,11 @@
 import platform
 from sys import version as pyver
-
 import psutil
 from pyrogram import __version__ as pyrover
 from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
-
 import config
 from AnonXMusic import app
 from AnonXMusic.core.userbot import assistants
@@ -17,7 +15,6 @@ from AnonXMusic.utils.database import get_served_chats, get_served_users, get_su
 from AnonXMusic.utils.decorators.language import language, languageCB
 from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
-
 
 @app.on_message(filters.command(["istatistik"]) & filters.group & ~BANNED_USERS)
 @language
@@ -29,7 +26,6 @@ async def stats_global(client, message: Message, _):
         reply_markup=upl,
     )
 
-
 @app.on_callback_query(filters.regex("stats_back") & ~BANNED_USERS)
 @languageCB
 async def home_stats(client, CallbackQuery, _):
@@ -38,7 +34,6 @@ async def home_stats(client, CallbackQuery, _):
         text=_["gstats_2"].format(app.mention),
         reply_markup=upl,
     )
-
 
 @app.on_callback_query(filters.regex("TopOverall") & ~BANNED_USERS)
 @languageCB
@@ -49,9 +44,10 @@ async def overall_stats(client, CallbackQuery, _):
         await CallbackQuery.answer()
     except:
         pass
-    await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
-    served_chats = len(await get_served_chats())
-    served_users = len(await get_served_users())
+
+    served_chats = len(await get_served_chats())  # Servis edilen sohbet sayısını alıyoruz
+    served_users = len(await get_served_users())  # Servis edilen kullanıcı sayısını alıyoruz
+
     text = _["gstats_3"].format(
         app.mention,
         len(assistants),
@@ -71,21 +67,21 @@ async def overall_stats(client, CallbackQuery, _):
             photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
         )
 
-
 @app.on_callback_query(filters.regex("bot_stats_sudo"))
 @languageCB
 async def bot_stats(client, CallbackQuery, _):
     if CallbackQuery.from_user.id not in SUDOERS:
         return await CallbackQuery.answer(_["gstats_4"], show_alert=True)
+    
     upl = back_stats_buttons(_)
     try:
         await CallbackQuery.answer()
     except:
         pass
-    await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
+    
     p_core = psutil.cpu_count(logical=False)
     t_core = psutil.cpu_count(logical=True)
-    ram = str(round(psutil.virtual_memory().total / (1024.0**3))) + " ɢʙ"
+    ram = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " ɢʙ"
     try:
         cpu_freq = psutil.cpu_freq().current
         if cpu_freq >= 1000:
@@ -94,15 +90,18 @@ async def bot_stats(client, CallbackQuery, _):
             cpu_freq = f"{round(cpu_freq, 2)}ᴍʜᴢ"
     except:
         cpu_freq = "𝗁𝖺𝗍𝖺"
+    
     hdd = psutil.disk_usage("/")
-    total = hdd.total / (1024.0**3)
-    used = hdd.used / (1024.0**3)
-    free = hdd.free / (1024.0**3)
+    total = hdd.total / (1024.0 ** 3)
+    used = hdd.used / (1024.0 ** 3)
+    free = hdd.free / (1024.0 ** 3)
+    
     call = await mongodb.command("dbstats")
     datasize = call["dataSize"] / 1024
     storage = call["storageSize"] / 1024
-    served_chats = len(await get_served_chats())
-    served_users = len(await get_served_users())
+    served_chats = len(await get_served_chats())  # Servis edilen sohbet sayısını alıyoruz
+    served_users = len(await get_served_users())  # Servis edilen kullanıcı sayısını alıyoruz
+    
     text = _["gstats_5"].format(
         app.mention,
         len(ALL_MODULES),
