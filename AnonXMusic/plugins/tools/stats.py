54e32905628a2b7926f7ab2d7ba443a7a7284943
AHMET1346z <145866626+AHMET1346z@users.noreply.github.com>
2024-10-11 19:02:10 +0300

    Update stats.py

--- AnonXMusic/plugins/tools/stats.py
+++ AnonXMusic/plugins/tools/stats.py
@@ -1,8 +1,9 @@
 import platform
-import asyncio
 from sys import version as pyver
+
 import psutil
-from pyrogram import __version__ as pyrover, Client, filters
+from pyrogram import __version__ as pyrover
+from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
@@ -12,9 +13,7 @@ from AnonXMusic import app
 from AnonXMusic.core.userbot import assistants
 from AnonXMusic.misc import SUDOERS, mongodb
 from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import (
-    get_served_chats, get_served_users, add_served_chat, add_served_user, get_sudoers
-)
+from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
 from AnonXMusic.utils.decorators.language import language, languageCB
 from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
@@ -51,7 +50,6 @@ async def overall_stats(client, CallbackQuery, _):
     except:
         pass
     await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
-    
     served_chats = len(await get_served_chats())
     served_users = len(await get_served_users())
     text = _["gstats_3"].format(
@@ -79,18 +77,15 @@ async def overall_stats(client, CallbackQuery, _):
 async def bot_stats(client, CallbackQuery, _):
     if CallbackQuery.from_user.id not in SUDOERS:
         return await CallbackQuery.answer(_["gstats_4"], show_alert=True)
-    
     upl = back_stats_buttons(_)
     try:
         await CallbackQuery.answer()
     except:
         pass
-    
     await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
     p_core = psutil.cpu_count(logical=False)
     t_core = psutil.cpu_count(logical=True)
     ram = str(round(psutil.virtual_memory().total / (1024.0**3))) + " ɢʙ"
-    
     try:
         cpu_freq = psutil.cpu_freq().current
         if cpu_freq >= 1000:
@@ -99,18 +94,15 @@ async def bot_stats(client, CallbackQuery, _):
             cpu_freq = f"{round(cpu_freq, 2)}ᴍʜᴢ"
     except:
         cpu_freq = "𝗁𝖺𝗍𝖺"
-    
     hdd = psutil.disk_usage("/")
     total = hdd.total / (1024.0**3)
     used = hdd.used / (1024.0**3)
     free = hdd.free / (1024.0**3)
-    
     call = await mongodb.command("dbstats")
     datasize = call["dataSize"] / 1024
     storage = call["storageSize"] / 1024
     served_chats = len(await get_served_chats())
     served_users = len(await get_served_users())
-    
     text = _["gstats_5"].format(
         app.mention,
         len(ALL_MODULES),
@@ -134,7 +126,6 @@ async def bot_stats(client, CallbackQuery, _):
         call["collections"],
         call["objects"],
     )
-    
     med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
     try:
         await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
@@ -142,34 +133,3 @@ async def bot_stats(client, CallbackQuery, _):
         await CallbackQuery.message.reply_photo(
             photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
         )
-
-
-# Yeni grupları ve kullanıcıları 3 saatte bir tarayıp veritabanına ekleyen fonksiyon
-async def scan_and_update_database():
-    async with app:
-        while True:
-            dialogs = await app.get_dialogs()  # Tüm sohbetleri al
-            current_chats = await get_served_chats()  # Zaten kaydedilmiş sohbetler
-            
-            for dialog in dialogs:
-                if dialog.chat.type in ["supergroup", "group", "channel"]:  # Sadece grup, süpergrup ve kanalları kaydet
-                    chat_id = dialog.chat.id
-                    
-                    if chat_id not in current_chats:  # Eğer veritabanında yoksa
-                        await add_served_chat(chat_id)  # Grubu veritabanına kaydet
-                        
-                    # Grubun tüm kullanıcılarını tespit et
-                    participants = await app.get_chat_members(chat_id)
-                    
-                    for participant in participants:
-                        user_id = participant.user.id
-                        await add_served_user(user_id)  # Kullanıcıyı veritabanına kaydet
-                        
-            print("Tarama tamamlandı. 3 saat sonra tekrar tarama yapılacak.")
-            
-            await asyncio.sleep(10800)  # 3 saat bekle (10800 saniye)
-
-
-# Bot başlatılırken tarama başlatılsın
-loop = asyncio.get_event_loop()
-loop.run_until_complete(scan_and_update_database())
